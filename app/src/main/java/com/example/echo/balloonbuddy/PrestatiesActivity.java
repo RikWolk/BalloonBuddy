daa17511f8d25308cbe296785792006fad363d0d
Thiefje <mariusstolp@hotmail.com>
2018-12-10 14:56:03 +0100

    Navigatie geupdate met icons. Prestatiescherm uitgebreid.

--- app/src/main/java/com/example/echo/balloonbuddy/PrestatiesActivity.java
+++ app/src/main/java/com/example/echo/balloonbuddy/PrestatiesActivity.java
@@ -1,7 +1,11 @@
 package com.example.echo.balloonbuddy;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class PrestatiesActivity extends AppCompatActivity {
 
@@ -9,5 +13,29 @@ public class PrestatiesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_prestaties);
+
+        ImageButton settingsButton;
+        ImageButton prestatiesButton;
+
+        settingsButton = (ImageButton) findViewById(R.id.settingsButton);
+        prestatiesButton = (ImageButton) findViewById(R.id.prestatiesButton);
+
+        settingsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(PrestatiesActivity.this, SettingsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        prestatiesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(PrestatiesActivity.this, PrestatiesActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
+
 }
