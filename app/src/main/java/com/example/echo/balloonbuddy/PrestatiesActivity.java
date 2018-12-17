da79296616eaaf9bac26f8e3cfe3a1e02e440586
Thiefje <mariusstolp@hotmail.com>
2018-12-17 11:38:03 +0100

    Navigatie aangepast van settings & prestaties en terug naar Main. Begin ontwerp layout GameActivity.

--- app/src/main/java/com/example/echo/balloonbuddy/PrestatiesActivity.java
+++ app/src/main/java/com/example/echo/balloonbuddy/PrestatiesActivity.java
@@ -4,9 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
-import android.widget.ImageView;
 
 public class PrestatiesActivity extends AppCompatActivity {
 
@@ -16,10 +14,10 @@ public class PrestatiesActivity extends AppCompatActivity {
         setContentView(R.layout.activity_prestaties);
 
         ImageButton settingsButton;
-        ImageButton prestatiesButton;
+        ImageButton homeButton;
 
-        settingsButton = (ImageButton) findViewById(R.id.settingsButton);
-        prestatiesButton = (ImageButton) findViewById(R.id.prestatiesButton);
+        settingsButton = (ImageButton) findViewById(R.id.pauseButton);
+        homeButton = (ImageButton) findViewById(R.id.pauseButton);
 
         settingsButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -29,15 +27,14 @@ public class PrestatiesActivity extends AppCompatActivity {
             }
         });
 
-        prestatiesButton.setOnClickListener(new View.OnClickListener() {
+        homeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(PrestatiesActivity.this, PrestatiesActivity.class);
+                Intent intent = new Intent(PrestatiesActivity.this, MainActivity.class);
                 startActivity(intent);
             }
         });
 
-        int[] i = {R.mipmap.icons_beker_locked_v01, R.mipmap.icons_beker_unlocked_v01};
 
     }
 
