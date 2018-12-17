3f126339afad6d9826bebb7987996f7138af2540
Lex van der Werff <lex@dhcp-145.101.83.181.pool.hsleiden.nl>
2018-12-17 11:23:53 +0100

    Push notficaties werkend

--- app/src/main/java/com/example/echo/balloonbuddy/MainActivity.java
+++ app/src/main/java/com/example/echo/balloonbuddy/MainActivity.java
@@ -1,11 +1,16 @@
 package com.example.echo.balloonbuddy;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -45,5 +50,15 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        // LEX TEST MET LOCAL NOTIFICATIONS
+        AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+
+        Intent notificationIntent = new Intent(this, com.example.echo.balloonbuddy.AlarmReceiver.class);
+        PendingIntent broadcast = PendingIntent.getBroadcast(this, 100, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        Calendar cal = Calendar.getInstance();
+        cal.add(Calendar.SECOND, 5);
+        alarmManager.setExact(AlarmManager.RTC_WAKEUP, cal.getTimeInMillis(), broadcast);
     }
 }
