313b2022b1883121065f23b093e664a8b82409cc
Lex van der Werff <lex@dhcp-145.101.83.181.pool.hsleiden.nl>
2018-12-17 11:23:53 +0100

    Push notficaties werkend

--- app/src/main/java/com/example/echo/balloonbuddy/MainActivity.java
+++ app/src/main/java/com/example/echo/balloonbuddy/MainActivity.java
@@ -1,7 +1,16 @@
 package com.example.echo.balloonbuddy;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +18,47 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageButton settingsButton;
+        ImageButton prestatiesButton;
+        Button startButton;
+
+        settingsButton = (ImageButton) findViewById(R.id.settingsButton);
+        prestatiesButton = (ImageButton) findViewById(R.id.prestatiesButton);
+        startButton = (Button) findViewById(R.id.startButton);
+
+        settingsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        prestatiesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, PrestatiesActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, GameActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // LEX TEST MET LOCAL NOTIFICATIONS
+        AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+
+        Intent notificationIntent = new Intent(this, com.example.echo.balloonbuddy.AlarmReceiver.class);
+        PendingIntent broadcast = PendingIntent.getBroadcast(this, 100, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        Calendar cal = Calendar.getInstance();
+        cal.add(Calendar.SECOND, 5);
+        alarmManager.setExact(AlarmManager.RTC_WAKEUP, cal.getTimeInMillis(), broadcast);
     }
-    // Ik ben Daan
 }
---
+++ app/src/main/java/com/example/echo/balloonbuddy/NotificationActivity.java
@@ -0,0 +1,6 @@
+package com.example.echo.balloonbuddy;
+
+public class NotificationActivity {
+
+}
+
