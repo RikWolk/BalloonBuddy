fb5a2ce0f59fcc6960b774b8a97dff2ed13e52ba
Thiefje <mariusstolp@hotmail.com>
2018-12-19 12:06:57 +0100

    - Home icon altijd links in settings & prestaties menus. - Fixed stacking settings & prestaties.

--- app/src/main/java/com/example/echo/balloonbuddy/SettingsActivity.java
+++ app/src/main/java/com/example/echo/balloonbuddy/SettingsActivity.java
@@ -29,6 +29,7 @@ public class SettingsActivity extends AppCompatActivity {
         prestatiesButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                finish();
                 Intent intent = new Intent(SettingsActivity.this, PrestatiesActivity.class);
                 startActivity(intent);
             }
