bd4b6a90d6f4bf0f1fd1eefdfdf21aec480f83c3
Lex van der Werff <lex@dhcp-145.136.225.243.pool.hsleiden.nl>
2019-01-14 16:26:41 +0100

    GameTimer Class aangemaakt

---
+++ app/src/main/java/com/example/echo/balloonbuddy/GameTimer.java
@@ -0,0 +1,19 @@
+package com.example.echo.balloonbuddy;
+
+import android.os.CountDownTimer;
+
+public class GameTimer extends CountDownTimer {
+    public GameTimer() {
+        super(10000, 1000);
+    }
+
+    @Override
+    public void onFinish() {
+
+    }
+
+    @Override
+    public void onTick(long l) {
+
+    }
+}
